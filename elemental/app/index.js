710c98731ffe60ba078e21c559b817ac7fe22554
C-D-Lewis <Bonsitm@gmail.com>
2019-07-28 13:04:59 +0100

    Code update for elemental, SDK 3.1, gemini

--- elemental/app/index.js
+++ elemental/app/index.js
@@ -1,28 +1,28 @@
-const document = require('document');
-const clock = require('clock');
-
-import * as ui from '../common/ui';
-import * as helpers from '../common/helpers';
+import document from 'document';
+import clock from 'clock';
 
 const MONTHS = [
-  'January', 'February', 'March', 'April', 'May', 'June', 
+  'January', 'February', 'March', 'April', 'May', 'June',
   'July', 'August', 'September', 'October', 'November', 'December'
 ];
 
-const buildDate = (date) => {
-  const day = date.getDate();
-  const month = MONTHS[date.getMonth()];
-  return `${month} ${day}`;
-};
+const setText = (id, text) => document.getElementById(id).text = text;
+
+const zeroPad = value => (value < 10) ? `0${value}` : value;
+
+const formatDateStr = date => `${MONTHS[date.getMonth()]} ${date.getDate()}`;
 
 const onTick = (date) => {
-  const timeString = `${helpers.zeroPad(date.getHours())}:${helpers.zeroPad(date.getMinutes())}`;
-  ui.setText('time', timeString);
-  ui.setText('date', buildDate(date));
+  const timeString = `${zeroPad(date.getHours())}:${zeroPad(date.getMinutes())}`;
+  setText('time', timeString);
+  setText('date', formatDateStr(date));
 };
 
-(() => {
-  clock.granularity = 'minutes'; 
+const main = () => {
+  clock.granularity = 'minutes';
   clock.ontick = (evt) => onTick(evt.date);
+
   onTick(new Date());
-})();
+};
+
+main();
