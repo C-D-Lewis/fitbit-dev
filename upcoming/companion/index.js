92ca2ad07d4a9c8cc7d9a4c73e367a8c689e7263
Chris Lewis <Bonsitm@gmail.com>
2020-01-02 21:22:24 +0000

    Begin work on using Calendar API

--- upcoming/companion/index.js
+++ upcoming/companion/index.js
@@ -1,103 +1,82 @@
 import { settingsStorage } from 'settings';
-
-import * as auth from './auth';
+import me from 'companion';
+import calendars from 'calendars';
 import * as comm from '../common/comm';
 import Constants from '../common/constants';
-import * as web from '../common/web';
 
-// Support multiple calendars one day
-// const fetchCalendars = (accessToken) => {
-//   const params = { access_token: accessToken };
-//   const url = `https://www.googleapis.com/calendar/v3/users/me/calendarList?${web.buildParams(params)}`;
-//   return web.getJSON(url).then((json) => {
-//     if (json.error) throw new Error(json.error);
+const transformEvent = (item) => {
+  // Multiday allday
+  if (item.isAllDay) {
+    return {
+      title: item.title,
+      description: item.description || 'No description',
+      startDate: item.startDate.split('-').join('/'),
+      endDate: item.endDate.split('-').join('/'),
+      startTime: '00:00',
+      endTime: '00:00',
+    };
+  }
 
-//     return json.items;
-//   });
-// };
-
-const fetchCalendarEvents = (accessToken, calendarId = 'primary') => {
-  const params = {
-    access_token: accessToken,
-    timeMin: new Date().toISOString(),
-    orderBy: 'startTime',
-    singleEvents: 'true',
-    maxResults: Constants.maxEvents,
+  const [d1, t1] = item.startDate.split('T');
+  const [d2, t2] = item.endDate.split('T');
+  return {
+    title: item.title,
+    description: item.description || 'No description',
+    startDate: d1.split('-').join('/'),
+    endDate: d2.split('-').join('/'),
+    startTime: t1.split('+')[0].split(':').slice(0, 2).join(':'),
+    endTime: t2.split('+')[0].split(':').slice(0, 2).join(':'),
   };
-  const paramStr = web.buildParams(params);
-  const url = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?${paramStr}`;
-  return web.getJSON(url)
-    .then((json) => {
-      if (json.error) {
-        throw new Error(json.error);
-      }
-
-      console.log(`Got ${json.items.length} events from ${calendarId}`);
-      return json.items.map((item) => {
-        // Multiday allday
-        if (item.start.date) {
-          return {
-            title: item.summary,
-            description: item.description || 'No description',
-            startDate: item.start.date.split('-').join('/'),
-            endDate: item.end.date.split('-').join('/'),
-            startTime: '00:00',
-            endTime: '00:00',
-          };
-        }
-
-        const [d1, t1] = item.start.dateTime.split('T');
-        const [d2, t2] = item.end.dateTime.split('T');
-        return {
-          title: item.summary,
-          description: item.description || 'No description',
-          startDate: d1.split('-').join('/'),
-          endDate: d2.split('-').join('/'),
-          startTime: t1.split('+')[0].split(':').slice(0, 2).join(':'),
-          endTime: t2.split('+')[0].split(':').slice(0, 2).join(':'),
-        };
-      });
-    });
 };
 
-const getEvents = (oauthData, lastAttempt) => {
-  // TODO - choice of calendars in the JSX - forced to 'primary' for now
-  fetchCalendarEvents(oauthData.access_token, 'primary')
-    .then((eventList) => {
-      console.log(`Packet size: ${JSON.stringify(eventList).length}`);
-      comm.sendFile({ eventList });
-    })
-    .catch((err) => {
-      console.log(err.message);
-      if (lastAttempt) {
-        console.log('Gave up reauthenticating');
-        comm.sendFile({ error: err.message || 'Download Failed' });
-        return;
-      }
+const sendEventList = (eventList) => {
+  console.log(`Packet size: ${JSON.stringify(eventList).length}`);
+  comm.sendFile({ eventList });
+};
 
-      auth.refreshAccess(oauthData).then((newOauthData) => {
-        if (!newOauthData) {
-          console.log('Reauthentication Failed');
-          comm.sendFile({ error: 'Please log in' });
-          return;
-        }
+const handleSendError = (err) => {
+  console.log(err.stack);
+  comm.sendFile({ error: 'Please grant permissions and choose a calendar' });
+};
 
-        getEvents(newOauthData, true);
-      });
-  });
+const fetchCalendarEvents = () => calendars
+  .searchSources()
+  .then((sources) => {
+    settingsStorage.setItem('sources', JSON.stringify(sources));
+
+    return calendars.searchCalendars();
+  })
+  .then((results) => {
+    settingsStorage.setItem('calendars', JSON.stringify(results));
+
+    const startDate = new Date();
+    const endDate = new Date();
+    startDate.setHours(0, 0, 0, 0);
+    endDate.setHours(999, 59, 59, 999);
+    return calendars.searchEvents({ startDate, endDate });
+  })
+  .then(events => events.slice(0, Constants.maxEvents))
+  .then((events) => {
+    console.log('All events: ' + JSON.stringify(events));
+    return events;
+  })
+  .then(events => events.map(transformEvent));
+
+const getEvents = async () => {
+  fetchCalendarEvents()
+    .then(sendEventList)
+    .catch(handleSendError);
 };
 
 const onSocketOpen = () => {
   console.log('Companion peerSocket onopen');
 
-  const oauthData = auth.getOauthData();
-  if (!oauthData) {
-    console.log('No credentials for watch, notifying.');
-    comm.sendFile({ error: 'Please log in' });
+  if (!me.permissions.granted('access_calendar')) {
+    comm.sendFile({ error: 'Please grant permissions and choose a calendar' });
     return;
   }
 
-  getEvents(oauthData);
+  getEvents();
 };
 
 const main = () => {
@@ -106,11 +85,7 @@ const main = () => {
   comm.setup({ open: onSocketOpen });
 
   settingsStorage.onchange = (event) => {
-    if (event.key !== 'oauth') {
-      return;
-    }
-
-    getEvents(auth.getOauthData());
+    // Save prefs here?
   };
 };
 
--- upcoming/settings/index.jsx
+++ upcoming/settings/index.jsx
@@ -1,28 +1,9 @@
-import { settingsStorage } from 'settings';
-import Secrets from '../common/secrets';
-
 const mySettings = props => (
   <Page>
     <Section
-      title="Google Account">
-      The next 10 events from your primary calendar will be synchronized. If problems occur, try doing this again.
-
-      Note: This page will remain the same after login so you can come back and choose a different account later on.
-      <Oauth
-        settingsKey="oauth"
-        title="Choose Google Account"
-        label="Google Account"
-        status="Login"
-        authorizeUrl="https://accounts.google.com/o/oauth2/v2/auth"
-        requestTokenUrl="https://www.googleapis.com/oauth2/v4/token"
-        clientId={Secrets.clientId}
-        clientSecret={Secrets.clientSecret}
-        scope="https://www.googleapis.com/auth/calendar.readonly"
-        oAuthParams={{ access_type: 'offline', prompt: 'consent' }}
-        encodeAsBase64
-        pkce
-      />
+      title="Calendar Selection">
     </Section>
+    
   </Page>
 );
 
