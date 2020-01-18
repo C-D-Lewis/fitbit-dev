56ddbe7097c334990092171a95dc134d0f789230
C-D-Lewis <Bonsitm@gmail.com>
2020-01-18 12:14:37 +0000

    Update UI utils

--- utils/modules/ui.js
+++ utils/modules/ui.js
@@ -14,7 +14,19 @@ export const get = id => (typeof id === 'object') ? id : document.getElementById
  * @param {string} id - Element 'id' to show/hide.
  * @param {boolean} visible - Set to 'true' to show, 'false' to hide.
  */
-export const setVisible = (id, visible) => get(id).style.display = visible ? 'inline' : 'none';
+export const setVisible = (id, visible) => {
+  get(id).style.display = visible ? 'inline' : 'none';
+};
+
+/**
+ * Set the 'text' of an element.
+ *
+ * @param {string} id - Element 'id' to update.
+ * @param {string} text - Text to show.
+ */
+export const setText = (id, text) => {
+  get(id).text = text;
+};
 
 /**
  * Trigger an element's associated animation.
@@ -24,20 +36,23 @@ export const setVisible = (id, visible) => get(id).style.display = visible ? 'in
 export const animate = id => get(id).animate('enable');
 
 /**
- * Windows collect multiple elements by ID, and start hidden
- * setupCb is called immediately
- * updateCb is called at the developer's discretion
+ * Windows collect multiple elements by ID using a parent <svg> element, and start hidden.
+ * opts.setup is called immediately
+ * opts.update is called at the developer's discretion to update all elements at once.
+ *
+ * @param {Object} opts - Options (see below)
+ * @returns {Function} Window 'class'.
  */
-export const Window = (ids, setupCb, updateCb) => {
-  this.ids = ids;
-  this.show = () => this.ids.forEach(id => setVisible(id, true));
-  this.hide = () => this.ids.forEach(id => setVisible(id, false));
+export function Window({ id, setup, update }) {
+  this.id = id;
+  this.show = () => setVisible(this.id, true);
+  this.hide = () => setVisible(this.id, false);
   this.hide();
 
-  if (updateCb) {
-    this.update = updateCb;
+  if (update) {
+    this.update = update;
   }
-  if (setupCb) {
-    setupCb();
+  if (setup) {
+    setup();
   }
-};
+}
