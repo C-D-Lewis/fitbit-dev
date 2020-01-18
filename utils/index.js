0b96da2abf5fcd40df708769a22942cc478d831c
C-D-Lewis <Bonsitm@gmail.com>
2020-01-18 13:15:58 +0000

    Separate util modules by device

--- utils/index.js
+++
@@ -1,5 +0,0 @@
-export * as AOD from './modules/aod';
-export * as Comm from './modules/comm';
-export * as DB from './modules/db';
-export * as UI from './modules/ui';
-export * as Web from './modules/web';
