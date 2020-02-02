19e063200ba4d6e90aa088b423466d179388b5db
C-D-Lewis <Bonsitm@gmail.com>
2020-02-02 13:09:43 +0000

    1.0.20

--- utils/src/app/ui.js
+++ utils/src/app/ui.js
@@ -36,7 +36,10 @@ export const setText = (id, text) => {
 export const animate = id => get(id).animate('enable');
 
 /**
- * Windows collect multiple elements by ID using a parent <svg> element, and start hidden.
+ * Windows collect multiple elements by ID using a parent <svg> element,
+ * allow showing/hiding of the whole tree in one go, provide update routines,
+ * and start hidden.
+ *
  * opts.setup is called immediately
  * opts.update is called at the developer's discretion to update all elements at once.
  *
@@ -56,3 +59,57 @@ export function Window({ id, setup, update }) {
     setup();
   }
 }
+
+/**
+ * Get a child element of a parent by its own ID.
+ *
+ * @param {HTMLElement} element - Element object parent.
+ * @param {string} id - ID of the child element.
+ * @returns {HTMLElement} Child element.
+ */
+const getChild = (element, id) => element.getElementById(id);
+
+/**
+ * Cards are a single instance, multiple reuse element with child elements.
+ * Associated SVG is not included, but goes something like this:
+ *
+ *  <defs>
+ *    <symbol id="card">
+ *      <image id="bg" class="card-bg" href="./resources/bg-rounded.png"/>
+ *      <text id="index" class="card-index">0/0</text>
+ *      <use href="#panoramaview">
+ *        <use href="#panoramaview-item">
+ *          <textarea id="title" class="card-title">TITLE</textarea>
+ *        </use>
+ *        <use href="#panoramaview-item">
+ *          <textarea id="description" class="card-description">DESC</textarea>
+ *        </use>
+ *
+ *       <use id="pagination-dots" href="#pagination-widget" class="card-dots">
+ *          <use href="#pagination-dot"/>
+ *          <use href="#pagination-dot"/>
+ *          <use href="#pagination-highlight-dot"/>
+ *        </use>
+ *      </use>
+ *      <rect class="card-div-line"/>
+ *      <text id="date" class="card-date">date</text>
+ *    </symbol>
+ *  </defs>
+ *
+ * And used as so:
+ *
+ *  <use href="#tile-list-item">
+ *    <use id="card[0]" href="#card"/>
+ *  </use>
+ */
+export function Card(id) {
+  this.root = get(id);
+
+  this.setText = function (id, text) {
+    getChild(this.root, id).text = text;
+  };
+
+  this.get = function (id) {
+    return getChild(this.root, id);
+  };
+};
