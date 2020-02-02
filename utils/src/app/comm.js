db8320a0af495e5ebd0462c9ac653d0b2919d320
C-D-Lewis <Bonsitm@gmail.com>
2020-02-02 12:36:00 +0000

    1.0.16

--- utils/src/app/comm.js
+++ utils/src/app/comm.js
@@ -1,30 +1,28 @@
-// Problems when file() is uncommented, not sure why
+import { inbox } from 'file-transfer';
+import { readFileSync } from 'fs';
+import * as messaging from 'messaging';
 
-// import { inbox } from 'file-transfer';
-// import { readFileSync } from 'fs';
-// import * as messaging from 'messaging';
-//
-// /**
-//  * Setup handlers for messaging and file-transfer events.
-//  *
-//  * @param {Object} handlers - Handlers for socket open, socket message,
-//  *                            errors, and file received events.
-//  */
-// export const setup = (handlers) => {
-//   const { open, message, error, file } = handlers;
-//
-//   messaging.peerSocket.onopen = open;
-//   messaging.peerSocket.onmessage = message;
-//   messaging.peerSocket.onerror = error;
-//
-//   try {
-//     inbox.onnewfile = () => {
-//       const fileName = inbox.nextFile();
-//       const json = readFileSync(`/private/data/${fileName}`, 'cbor');
-//       console.log(`File ${fileName} received`);
-//       file(fileName, json);
-//     };
-//   } catch (e) {
-//     console.log(e);
-//   }
-// };
+/**
+ * Setup handlers for messaging and file-transfer events.
+ *
+ * @param {Object} handlers - Handlers for socket open, socket message,
+ *                            errors, and file received events.
+ */
+export const setup = (handlers) => {
+  const { open, message, error, file } = handlers;
+
+  messaging.peerSocket.onopen = open;
+  messaging.peerSocket.onmessage = message;
+  messaging.peerSocket.onerror = error;
+
+  try {
+    inbox.onnewfile = () => {
+      const fileName = inbox.nextFile();
+      const json = readFileSync(`/private/data/${fileName}`, 'cbor');
+      console.log(`File ${fileName} received`);
+      file(fileName, json);
+    };
+  } catch (e) {
+    console.log(e);
+  }
+};
