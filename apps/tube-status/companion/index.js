f704877fc02f931391d36d413443e01dea6b20b5
C-D-Lewis <Bonsitm@gmail.com>
2021-09-11 13:03:45 +0100

    Update design for 336x336

--- apps/tube-status/companion/index.js
+++ apps/tube-status/companion/index.js
@@ -1,12 +1,28 @@
 import { Comm } from '@chris-lewis/fitbit-utils/companion';
-import * as status from './status';
+import { downloadStatus } from './status';
 
+/**
+ * The main function.
+ */
 const main = () => {
   console.log('Tube Status companion start');
 
   Comm.setup({
-    open: () => status.download().then(lines => Comm.sendFile(lines)),
+    /**
+     * When communication opens.
+     */
+    open: () => downloadStatus().then(lines => Comm.sendFile(lines)),
+    /**
+     * On message from the device.
+     *
+     * @param {object} event - Communication event.
+     */
     message: event => console.log(`RECV: ${JSON.stringify(event)}`),
+    /**
+     * On error.
+     *
+     * @param {object} err - Communication error.
+     */
     error: err => console.log(`Connection error: ${err.code} - ${err.message}`),
   });
 };
--- apps/tube-status/companion/status.js
+++ apps/tube-status/companion/status.js
@@ -1,13 +1,22 @@
+/** Max length of a reason */
 const REASON_MAX_LENGTH = 192;
 
-const getAllLinesStatus = json => json.map((item) => {
+/**
+ * Extract all line statuses and reasons.
+ *
+ * @param {Array<object>} arr - Array of status items.
+ * @returns {Array<object>} Mapped status items.
+ */
+const getAllLinesStatus = arr => arr.map((item) => {
+  const [status] = item.lineStatuses;
   const result = {
     id: item.id,
-    status: item.lineStatuses[0].statusSeverityDescription,
+    status: status.statusSeverityDescription,
+    reason: undefined,
   };
 
-  if (item.lineStatuses[0].reason) {
-    let reason = item.lineStatuses[0].reason.substring(0, REASON_MAX_LENGTH);
+  if (status.reason) {
+    let reason = status.reason.substring(0, REASON_MAX_LENGTH);
     if (reason.includes(':')) {
       reason = reason.substring(reason.indexOf(':') + 1);
     }
@@ -18,11 +27,17 @@ const getAllLinesStatus = json => json.map((item) => {
   return result;
 });
 
-export const download = () => {
+/**
+ * Download the status from TfL
+ *
+ * @returns {Promise<Array<object>>}
+ */
+export const downloadStatus = () => {
   return fetch('https://api.tfl.gov.uk/line/mode/tube,dlr,overground,tflrail/status')
     .then(res => res.json())
     .then((json) => {
       console.log('Download from unified API complete!');
+
       const payload = getAllLinesStatus(json);
       // console.log(JSON.stringify(payload));
       console.log(`payload.length=${JSON.stringify(payload).length}`);
